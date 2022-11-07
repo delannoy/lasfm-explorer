44a09f8637ac8af45044cc75e1df055fb2a2abe2
Andres G. Delannoy <delannoy@cern.ch>
2022-11-07 19:37:42 +0100

    common parameters

---
+++ lastfm/api/param.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+url: str = 'http://ws.audioscrobbler.com/2.0/'
+
+headers: dict[str, str] = {'User-Agent': 'delannoy/0.1 (a@delannoy.cc)'}
+
+sleep: float = 0.2 # [Rate limit copied from pylast since it is not explicitly mentioned in the API TOS](https://github.com/pylast/pylast/blob/master/src/pylast/__init__.py#L115)
