a99edc61240dea543336171be602671ee79e2c26
Andres G. Delannoy <delannoy@cern.ch>
2022-11-10 06:06:46 +0100

    added `params` dict with `user` and `api_key`

--- lastfm/api/param.py
+++ lastfm/api/param.py
@@ -8,4 +8,6 @@ headers: dict[str, str] = {'User-Agent': 'delannoy/0.1 (a@delannoy.cc)'}
 
 default: dict[str, str] = {'format': 'json'}
 
+params: typing.Dict[str, typing.Any] = {'user': 'USER', 'api_key': 'APIKEY'}
+
 sleep: float = 0.2 # [Rate limit copied from pylast since it is not explicitly mentioned in the API TOS](https://github.com/pylast/pylast/blob/master/src/pylast/__init__.py#L115)
