8ef030a65c7294fd3d668cd5c222f187368dabfe
Andres G. Delannoy <delannoy@cern.ch>
2022-12-11 18:54:06 +0100

    remove generic alias `array` function

--- lastfm/api/typ.py
+++ lastfm/api/typ.py
@@ -9,6 +9,3 @@ xml = xml.etree.ElementTree.Element
 response = typing.Union[json, xml]
 
 uuid = typing.Union[str, uuid.UUID]
-
-def array(t: type = None) -> typing._GenericAlias:
-    return typing.Union[typing.List[t], typing.Set[t], typing.Tuple[t]]
