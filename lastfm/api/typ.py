7b826fabfa51c41c055cc5582834fb0c61ce0041
Andres G. Delannoy <delannoy@cern.ch>
2022-12-24 13:11:08 -0400

    added `pydantic` validator to coerce `str` values to `uuid`

--- lastfm/api/typ.py
+++ lastfm/api/typ.py
@@ -10,8 +10,12 @@ json = typing.Dict[str, typing.Any]
 xml = xml.etree.ElementTree.Element
 response = typing.Union[json, xml]
 
+# typevar = typing.TypeVar('typevar')
+# array = typing.Union[typing.List[typevar], typing.Set[typevar], typing.Tuple[typevar]]
+
 lang = pydantic.typing.Annotated[str, pydantic.Field(min_length=3, max_length=3)]
 tags = pydantic.typing.Annotated[typing.List[str], pydantic.Field(max_items=10)]
+# tags = pydantic.typing.Annotated[array[str], pydantic.Field(max_items=10)]
 
 
 class UUID(uuid.UUID):
@@ -21,3 +25,15 @@ class UUID(uuid.UUID):
 
     def __repr__(self):
         return f'{self.__module__}.{self.__class__.__name__}({uuid.UUID.__str__(self)!r})'
+
+    @classmethod
+    def __get_validators__(cls):
+        # https://docs.pydantic.dev/usage/types/#custom-data-types
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if isinstance(v, str):
+            return cls(v)
+        else:
+            return v
