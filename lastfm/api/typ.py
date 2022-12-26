67eb55e50017a054dd406085cc68f54d651500bd
Andres G. Delannoy <delannoy@cern.ch>
2022-12-25 22:20:22 -0400

    added `UnixTimestamp` type to parse/validate datetimes and convert to unix timestamps

--- lastfm/api/typ.py
+++ lastfm/api/typ.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import datetime
 import typing
 import uuid
 import xml.etree.ElementTree
 
+import dateparser
 import pydantic
 
 json = typing.Dict[str, typing.Any]
@@ -16,6 +18,29 @@ response = typing.Union[json, xml]
 lang = pydantic.typing.Annotated[str, pydantic.Field(min_length=3, max_length=3)]
 tags = pydantic.typing.Annotated[typing.List[str], pydantic.Field(max_items=10)]
 # tags = pydantic.typing.Annotated[array[str], pydantic.Field(max_items=10)]
+
+
+class UnixTimestamp:
+
+    def parseDateTime(dt: str, timezone: str = 'UTC') -> datetime.datetime:
+        return dateparser.parse(str(dt), settings={'TIMEZONE': timezone}) # [Python 3.9 PytzUsageWarning](https://github.com/scrapinghub/dateparser/issues/1013#issuecomment-1109403189)
+
+    def unixtimestamp(dt: datetime.datetime) -> int:
+        return int(dt.timestamp())
+
+    @classmethod
+    def __get_validators__(cls):
+        # https://docs.pydantic.dev/usage/types/#custom-data-types
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if isinstance(v, str):
+            return cls.unixtimestamp(cls.parseDateTime(v))
+        elif isinstance(v, datetime.datetime):
+            return cls.unixtimestamp(v)
+        else:
+            return v
 
 
 class UUID(uuid.UUID):
