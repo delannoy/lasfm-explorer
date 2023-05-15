2bae30a8219da6c5e8a39171800e97225a37e727
Andres G. Delannoy <delannoy@cern.ch>
2023-05-15 19:42:30 +0200

    moved logging config to `log.py`

---
+++ lastfm/log.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import logging
+
+import rich.logging
+
+log_level = logging.DEBUG
+
+# [Logging Handler](https://rich.readthedocs.io/en/stable/logging.html)
+handler = rich.logging.RichHandler(rich_tracebacks=True, log_time_format="[%Y-%m-%d %H:%M:%S]") # [rich.logging](https://rich.readthedocs.io/en/stable/reference/logging.html)
+logging.basicConfig(level=log_level, format='%(message)s', handlers=[handler])
+log = logging.getLogger()
+
+logging.getLogger('parso').setLevel(logging.WARNING) # https://github.com/ipython/ipython/issues/10946#issuecomment-568336466
