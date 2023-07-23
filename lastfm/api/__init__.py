9ff3c3c0840c60093e73abc71ef5b0b2a05c0337
Andres G. Delannoy <delannoy@cern.ch>
2023-07-23 23:28:20 +0200

    removed auth from `__all__`

--- lastfm/api/__init__.py
+++ lastfm/api/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
 import api.auth
-__all__ = ['auth', 'album', 'artist', 'chart', 'geo', 'library', 'tag', 'track', 'user']
+__all__ = ['album', 'artist', 'chart', 'geo', 'library', 'tag', 'track', 'user']
