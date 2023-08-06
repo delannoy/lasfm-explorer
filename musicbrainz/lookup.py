12cdbc388c58c113fb4f9b22c62dbb88645b599b
Andres G. Delannoy <delannoy@cern.ch>
2023-08-06 17:55:54 +0200

    implemented entity subquery for collection lookup and clean up arguments for non-MBID lookups

--- musicbrainz/lookup.py
+++ musicbrainz/lookup.py
@@ -13,11 +13,12 @@ import request
 def genreAll(limit: int = None, offset: int = 0, fmt: str = None) -> awkward.Record:
     return request.get(endpoint='/genre/all', limit=limit, offset=offset)
 
-def lookup(entity: str, mbid: uuid.UUID, inc: list[str] = None, type: list[str] = None, status: list[str] = None) -> awkward.Record:
+def lookup(entity: str, mbid: uuid.UUID, inc: list[str] = None, sub_entity: str = '', type: list[str] = None, status: list[str] = None) -> awkward.Record:
+    endpoint = f'/{entity}/{mbid}' if not sub_entity else f'/{entity}/{mbid}/{sub_entity}'
     inc = str.join('+', inc) if inc else None
     type = str.join('|', type) if type else None
     status = str.join('|', status) if status else None
-    return request.get(endpoint=f'/{entity}/{mbid}', inc=inc, type=type, status=status)
+    return request.get(endpoint=endpoint, inc=inc, type=type, status=status)
 
 def area(mbid: uuid.UUID, inc: list[str] = None) -> awkward.Record:
     return lookup(entity='area', mbid=mbid, inc=inc)
@@ -25,8 +26,8 @@ def area(mbid: uuid.UUID, inc: list[str] = None) -> awkward.Record:
 def artist(mbid: uuid.UUID, inc: list[str] = None, type: list[str] = None, status: list[str] = None) -> awkward.Record:
     return lookup(entity='artist', mbid=mbid, inc=inc, type=type, status=status)
 
-def collection(mbid: uuid.UUID, inc: list[str] = None) -> awkward.Record:
-    return lookup(entity='collection', mbid=mbid, inc=inc)
+def collection(mbid: uuid.UUID, sub_entity: str = '', inc: list[str] = None) -> awkward.Record:
+    return lookup(entity='collection', sub_entity=sub_entity, mbid=mbid, inc=inc)
 
 def event(mbid: uuid.UUID, inc: list[str] = None) -> awkward.Record:
     return lookup(entity='event', mbid=mbid, inc=inc)
@@ -64,10 +65,15 @@ def work(mbid: uuid.UUID, inc: list[str] = None) -> awkward.Record:
 # [Non-MBID Lookups](https://musicbrainz.org/doc/MusicBrainz_API#Non-MBID_Lookups)
 
 def discid(discid: str, inc: list[str] = None, toc: list[int] = None, cdstubs: bool = True, media_format: str = 'CD') -> awkward.Record:
-    return request.get(endpoint=f'/discid/{discid}', inc=str.join('+', inc), toc=str.join('+', toc), cdstubs='no' if not cdstubs else '', **{'media-format': media_format})
+    inc = str.join('+', inc) if inc else None
+    toc = str.join('+', toc) if toc else None
+    cdstubs = 'no' if cdstubs is False else None
+    return request.get(endpoint=f'/discid/{discid}', inc=inc, toc=toc, cdstubs=cdstubs, **{'media-format': media_format})
 
 def isrc(isrc: str, inc: list[str] = None) -> awkward.Record:
-    return request.get(endpoint=f'/isrc/{isrc}', inc=str.join('+', inc))
+    inc = str.join('+', inc) if inc else None
+    return request.get(endpoint=f'/isrc/{isrc}', inc=inc)
 
 def iswc(iswc: str, inc: list[str] = None) -> awkward.Record:
-    return request.get(endpoint=f'/iswc/{iswc}', inc=str.join('+', inc))
+    inc = str.join('+', inc) if inc else None
+    return request.get(endpoint=f'/iswc/{iswc}', inc=inc)
