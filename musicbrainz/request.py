92b9fe5d00f59c409410362b9842164330dbfbd2
Andres G. Delannoy <delannoy@cern.ch>
2023-08-06 18:00:46 +0200

    split `get` function into two: `getRequest` instantiates the GET request, `response` handles the response

--- musicbrainz/request.py
+++ musicbrainz/request.py
@@ -25,16 +25,18 @@ def auth():
         opener = urllib.request.build_opener(auth_handler)
         return opener
 
-def get(endpoint: str, **kwargs) -> awkward.Record:
+def getRequest(endpoint: str, **params) -> urllib.request.Request:
     time.sleep(api.SLEEP)
     url = f'https://musicbrainz.org/ws/2{endpoint}'
     headers = {'User-Agent': api.USER_AGENT, 'Accept': 'application/json'}
-    params = {k: v for k, v in kwargs.items() if v is not None}
-    if (kwargs.get('inc') is not None) and ('user' in kwargs.get('inc')):
+    params = {k: v for k, v in params.items() if v is not None}
+    if (params.get('inc') is not None) and ('user' in params.get('inc')):
         params.update(dict(client=api.CLIENT))
         urllib.request.install_opener(auth())
     logging.debug(f'{endpoint} | {params}')
-    request = urllib.request.Request(url=f'{url}?{urllib.parse.urlencode(params)}', headers=headers)
+    return urllib.request.Request(method='GET', url=f'{url}?{urllib.parse.urlencode(params)}', headers=headers)
+
+def response(request: urllib.request.Request) -> awkward.Record:
     try:
         with urllib.request.urlopen(request) as response:
             return awkward.from_json(source=response.read().decode('utf-8'))
@@ -42,3 +44,7 @@ def get(endpoint: str, **kwargs) -> awkward.Record:
         logging.error(f'{http_error.status} | {http_error.reason} | {http_error.url}')
         if 'json' in http_error.headers.get('Content-Type', ''):
             logging.error(json.loads(http_error.read().decode('utf-8')).get('error'))
+
+def get(endpoint: str, **params) -> awkward.Record:
+    request = getRequest(endpoint=endpoint, **params)
+    return response(request=request)
