5ef5e4b2f5cc24f8510c16cf3a3726b5b95d4390
Andres G. Delannoy <delannoy@cern.ch>
2023-11-19 11:34:13 +0100

    removed `pandas` dependency, added `releaseDate` method

--- deezer/deezer.py
+++ deezer/deezer.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-import pandas
+from __future__ import annotations
+import json
+import urllib
 
-def relatedArtists(artistID: int = 2814) -> pandas.DataFrame:
-    # [Artist / Related](https://developers.deezer.com/api/artist/related)
-    return pandas.json_normalize(pandas.read_json(f'https://api.deezer.com/artist/{artistID}/related').data)
+def relatedArtists(artistID: int = 2814) -> dict[str, int]:
+    '''[Artist / Related](https://developers.deezer.com/api/artist/related)'''
+    response = urllib.request.urlopen('https://api.deezer.com/artist/2814/related').read().decode('utf-8')
+    return {artist.get('name'): artist.get('id') for artist in json.loads(response).get('data')}
+
+def releaseDate(albumID: int = 53318422) -> str:
+    '''[Album](https://developers.deezer.com/api/album)'''
+    response = urllib.request.urlopen(f'https://api.deezer.com/album/{albumID}').read().decode('utf-8')
+    return json.loads(response).get('release_date')
