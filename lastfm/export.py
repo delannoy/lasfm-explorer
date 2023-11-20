6003c1b341acebe7ba33b64a8f178089b3071a6b
Andres G. Delannoy <delannoy@cern.ch>
2023-11-20 18:57:40 +0100

    refactored into multiple classes and implemented async playcounts per year

--- lastfm/export.py
+++ lastfm/export.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 import asyncio
+import dataclasses
 import datetime
 import fileinput
 import functools
@@ -21,91 +22,170 @@ from api import user
 import log
 import param
 
+# to do:
+    # export in series (urllib) if `httpx` is unavailable?
+    # async/parallelize all years
+
 log.log.setLevel(logging.getLevelName('INFO'))
 
 PARAMS = {'method': 'user.getRecentTracks', 'api_key': auth.api_key, 'user': auth.user, 'format': 'json', 'limit': 1000}
 EXPORT_PATH = pathlib.Path(f"data/{PARAMS.get('user')}/RecentTracks")
 HTTPX_TIMEOUT = 60.0
-PROGRESS_COLS = (rich.progress.TextColumn(text_format='[bold blue]{task.fields[year]}-{task.fields[page]:03d}'),
-                rich.progress.BarColumn(bar_width=None),
-                rich.progress.TaskProgressColumn(text_format='[progress.percentage]{task.percentage:>3.1f}%'),
-                rich.progress.DownloadColumn(),
-                rich.progress.TransferSpeedColumn(),
-                rich.progress.TimeRemainingColumn())
+PROGRESS_COLS = (rich.progress.TextColumn(text_format='[bold blue]{task.description}'),
+                 rich.progress.BarColumn(bar_width=None),
+                 rich.progress.TaskProgressColumn(text_format='[progress.percentage]{task.percentage:>3.1f}%'),
+                 rich.progress.DownloadColumn(),
+                 rich.progress.TransferSpeedColumn(),
+                 rich.progress.TimeRemainingColumn())
 
 def yearRange(year: int) -> tuple[int, int]:
-    '''Calculate unix timestamps corresponding to the start and end of `year`.'''
+    '''Return unix timestamps corresponding to the start and end of `year`.'''
     FROM = int(datetime.datetime.fromisoformat(f'{year}-01-01T00:00:00+00:00').timestamp())
     TO = int(datetime.datetime.fromisoformat(f'{year}-12-31T23:59:59+00:00').timestamp())
     return (FROM, TO)
 
+
+@dataclasses.dataclass
+class Response:
+    url: httpx.URL
+    progress: rich.progress.Progress
+    task: rich.progress.Task
+
+    async def collect(self) -> dict[str, typing.Any]:
+        '''Stream async GET request with `rich.progress`.'''
+        data = bytearray()
+        async with httpx.AsyncClient(timeout=HTTPX_TIMEOUT).stream(method='GET', url=self.url, headers=param.headers) as response:
+            self.task.total = int(response.headers.get('Content-Length'))
+            async for chunk in response.aiter_bytes():
+                data.extend(chunk)
+                self.progress.update(task_id=self.task.id, completed=response.num_bytes_downloaded)
+        return json.loads(data.decode('utf-8'))
+
+    async def download(self) -> None:
+        '''Query `self.url`, remove `nowplaying` track from response, and write to disk.'''
+        response = await self.collect()
+        response['recenttracks']['track'] = [track for track in response.get('recenttracks').get('track') if not track.get('@attr')] # remove `nowplaying` track from response
+        filepath = pathlib.Path(f'{EXPORT_PATH}/{self.task.description}.json')
+        with filepath.open(mode='w') as out_file:
+            json.dump(obj=response, fp=out_file)
+
+
+class Playcount:
+
+    @staticmethod
+    async def annual(year: int) -> int:
+        '''Query playcount for `year`'''
+        FROM, TO = yearRange(year=year)
+        url = httpx.URL(url=param.url, params={**PARAMS, 'from': FROM, 'to': TO, 'page': 1, 'limit': 1})
+        async with httpx.AsyncClient(timeout=HTTPX_TIMEOUT) as async_client:
+            response = await async_client.get(url=url)
+        return int(response.json().get('recenttracks').get('@attr').get('total'))
+
+    @classmethod
+    async def overall(cls, begin_year: int, end_year: int) -> dict[str, int]:
+        '''Query playcount for all years between `begin_year` and `end_year`.'''
+        jobs = [functools.partial(cls.annual, year=year) for year in range(begin_year, end_year+1)]
+        playcount = await aiometer.run_all(jobs, max_per_second=1/param.sleep)
+        year = list(map(str, range(begin_year, end_year+1)))
+        return dict(zip(year, playcount))
+
+
+class Disk:
+
+    @staticmethod
+    def playcount(filepath_glob: str = '*json') -> int:
+        '''Calculate number of track plays on disk for files matching `filepath_glob`.'''
+        # [asyncio does not support asynchronous operations on the filesystem](https://github.com/python/asyncio/wiki/ThirdParty#filesystem)
+        with fileinput.input(files=EXPORT_PATH.glob(filepath_glob), mode='r') as files:
+            try:
+                return sum(len(json.loads(f).get('recenttracks').get('track')) for f in files)
+            except json.decoder.JSONDecodeError as error:
+                log.log.error(f'JSON decode error when reading exported files:\n{sorted(EXPORT_PATH.glob(filepath_glob))}\nplease remove incomplete/corrupted file(s)\n')
+                raise error
+
+    @classmethod
+    def exported(cls, year: int, api_playcount: int) -> bool:
+        '''Check if all files corresponding to `year` have been exported with the expected number of track plays.'''
+        filepath_glob = f'{year}*json'
+        if not list(EXPORT_PATH.glob(filepath_glob)):
+            return False
+        disk_playcount = cls.playcount(filepath_glob=filepath_glob)
+        log.log.info(f'{disk_playcount} plays already exported') if (disk_playcount == api_playcount) else log.log.warning(f'export incomplete\n{disk_playcount = }\n{api_playcount = }')
+        return disk_playcount == api_playcount
+
+
 def getURL(year: int) -> list[httpx.URL]:
     '''Return paginated URLs for the given `year`.'''
     FROM, TO = yearRange(year=year)
-    total_pages = math.ceil(int(user.getRecentTracks(user=PARAMS.get('user'), FROM=FROM, TO=TO, limit=1).attr.totalPages) / PARAMS.get('limit'))
+    total_pages = math.ceil(user.getRecentTracks(user=PARAMS.get('user'), FROM=FROM, TO=TO, limit=1).attr.totalPages / PARAMS.get('limit'))
     return [httpx.URL(url=param.url, params={**PARAMS, 'from': FROM, 'to': TO, 'page': page}) for page in range(1, total_pages+1)]
 
-async def download(url: httpx.URL, progress: rich.progress.Progress, task: rich.progress.Task):
-    '''Query `url`, remove `nowplaying` track from response, and write to disk.'''
-    year = task.fields.get('year')
-    page = task.fields.get('page')
-    filepath = pathlib.Path(f'{EXPORT_PATH}/{year}-{page:03d}.json')
-    async with httpx.AsyncClient(timeout=HTTPX_TIMEOUT) as async_client:
-        async with async_client.stream(method='GET', url=url, headers=param.headers) as response:
-            task.total = int(response.headers.get('Content-Length'))
-            data = bytes()
-            async for chunk in response.aiter_bytes():
-                data = data + chunk
-                progress.update(task_id=task.id, completed=response.num_bytes_downloaded)
-    response = json.loads(data.decode('utf-8'))
-    response['recenttracks']['track'] = [track for track in response.get('recenttracks').get('track') if not track.get('@attr')] # remove `nowplaying` track from response
-    with open(filepath, mode='w') as out_file:
-        json.dump(obj=response, fp=out_file)
+async def exportYear(year: int) -> None:
+    '''Export all last.fm data for `PARAMS[user]` during `year`.'''
+    urls = getURL(year=year)
+    progress = rich.progress.Progress(*PROGRESS_COLS)
+    _ = [progress.add_task(description=f'{year}-{page:03d}') for page, url in enumerate(urls, start=1)]
+    with rich.live.Live(progress):
+        jobs = [Response(url=url, progress=progress, task=progress.tasks[task_id]).download for task_id, url in enumerate(urls)]
+        await aiometer.run_all(jobs, max_per_second=1/param.sleep)
 
-def playsOnDisk(filepath_glob: str = '*json') -> int:
-    '''Calculate number of track plays on disk for files matching `filepath_glob`.'''
-    try:
-        with fileinput.input(files=pathlib.Path(EXPORT_PATH).glob(filepath_glob), mode='r') as files:
-            return sum(len(json.loads(f).get('recenttracks').get('track')) for f in files)
-    except json.decoder.JSONDecodeError as error:
-        log.log.error(f'JSON decode error when reading exported files:\n{sorted(pathlib.Path(EXPORT_PATH).glob(filepath_glob))}\nplease remove incomplete/corrupted file(s)\n')
-        raise error
-
-def validateExport(playcount: int, filepath_glob: str = '*json'):
-    '''Verify that the number of track plays for files matching `filepath_glob` is equal to `playcount`.'''
-    playcount_on_disk = playsOnDisk(filepath_glob=filepath_glob)
-    log.log.info(f'{playcount} plays already exported') if playcount_on_disk == playcount else log.log.warning(f'export incomplete\n{playcount = }\n{playcount_on_disk = }')
-    return playcount_on_disk == playcount
-
-def alreadyExported(year: int):
-    '''Check if all files corresponding to `year` have been exported with the expected number of track plays.'''
-    if not sorted(pathlib.Path(EXPORT_PATH).glob(f'{year}*json')):
-        return False
-    FROM, TO = yearRange(year=year)
-    playcount = int(user.getRecentTracks(user=PARAMS.get('user'), FROM=FROM, TO=TO, limit=1).attr.totalPages)
-    return validateExport(playcount=playcount, filepath_glob=f'{year}*.json')
-
-async def export(force: bool = False):
+async def export(force: bool = False) -> None:
     '''Export all last.fm data for `PARAMS['user']`.'''
-    total_playcount = int(user.getInfo(user=PARAMS.get('user')).playcount)
-    first_scrobble = user.getRecentTracks(user=PARAMS.get('user'), limit=1, page=total_playcount).track.date.dateTime[-1]
-    begin_year = datetime.datetime.fromisoformat(f'{first_scrobble}+00:00').year
+    playcount_total = user.getInfo(user=PARAMS.get('user')).playcount
+    begin_year = user.getRecentTracks(user=PARAMS.get('user'), limit=1, page=playcount_total).track[-1].date.dateTime.year
     current_year = datetime.datetime.now(tz=datetime.timezone.utc).year
+    playcount_per_year = await Playcount.overall(begin_year, current_year)
     for year in range(begin_year, current_year+1):
         rich.console.Console().rule(title=str(year))
-        if (not force) and alreadyExported(year=year):
+        if (not force) and Disk.exported(year=year, api_playcount=playcount_per_year.get(str(year))):
             continue
-        urls = getURL(year)
-        progress = rich.progress.Progress(*PROGRESS_COLS)
-        _ = [progress.add_task(description=f'{year}-{page:03d}', year=year, page=page) for page, url in enumerate(urls, start=1)]
-        with rich.live.Live(progress):
-            jobs = [functools.partial(download, url=url, progress=progress, task=progress.tasks[page]) for page, url in enumerate(urls)]
-            await aiometer.run_all(jobs, max_per_second=1/param.sleep)
+        await exportYear(year=year)
 
-def main():
-    '''Run data export asynchronously.'''
+def main() -> None:
+    '''Export data asynchronously.'''
     EXPORT_PATH.mkdir(parents=True, exist_ok=True)
     return asyncio.run(export())
+
+
+class Serialize:
+
+    @staticmethod
+    def readJSON() -> list:
+        with fileinput.input(files=EXPORT_PATH.glob('*json'), mode='r') as files:
+            return [track for f in files for track in json.loads(f).get('recenttracks').get('track')]
+
+    @classmethod
+    def akToParquet(cls):
+        import awkward
+        tracks = awkward.Array(cls.readJSON())
+        awkward.to_parquet(array=tracks, destination=pathlib.Path(EXPORT_PATH/'ak_tracks.parquet'))
+
+    @classmethod
+    def pdToFeather(cls):
+        import pandas
+        tracks = pandas.DataFrame(cls.readJSON())
+        tracks.to_feather(path=pathlib.Path(EXPORT_PATH/'pd_tracks.feather'))
+
+    @classmethod
+    def topTracks(cls):
+        import pandas
+        tracks = pandas.read_feather(EXPORT_PATH/'pd_tracks.feather') if (EXPORT_PATH/'pd_tracks.feather').exists() else pandas.DataFrame(cls.readJSON())
+        tracks = [pandas.json_normalize(tracks.artist)['#text'].rename('artist'), tracks.name.rename('track'), tracks.streamable.rename('playcount')]
+        top_tracks = pandas.concat(objs=tracks, axis=1).apply(lambda col: col.str.casefold())
+        top_tracks = top_tracks.groupby(['artist', 'track']).count().sort_values(by='playcount', ascending=False).reset_index()
+        return top_tracks
+
+    @classmethod
+    def _topTracks(cls):
+        import pandas
+        data = pandas.read_feather(EXPORT_PATH/'pd_tracks.feather') if (EXPORT_PATH/'pd_tracks.feather').exists() else pandas.DataFrame(cls.readJSON())
+        artists = pandas.json_normalize(data.artist).rename(columns={'#text': 'artist_name', 'mbid': 'artist_mbid'})
+        albums = pandas.json_normalize(data.album).rename(columns={'#text': 'album_name', 'mbid': 'album_mbid'})
+        tracks = data[['name', 'mbid', 'streamable']].rename(columns={'name': 'track_name', 'mbid': 'track_mbid', 'streamable': 'playcount'})
+        top_tracks = pandas.concat(objs=[artists, albums, tracks], axis=1).apply(lambda col: col.str.casefold())
+        top_tracks = top_tracks.groupby([col for col in top_tracks.columns if col != 'playcount']).count().sort_values(by='playcount', ascending=False).reset_index().replace('', None)
+        return top_tracks
+
 
 if __name__ == '__main__':
     main()
