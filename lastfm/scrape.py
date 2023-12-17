335aa24581363596bce3079cfab69aee147fb705
Andres G. Delannoy <delannoy@cern.ch>
2023-12-16 20:42:47 -0400

    print response as `rich.table`; move `Recommended` login-related class attributes to `Recommended.login`

--- lastfm/scrape.py
+++ lastfm/scrape.py
@@ -8,6 +8,8 @@ import os
 import httpx
 import lxml.html
 import pandas
+import rich.console
+import rich.table
 
 import api.auth
 import log
@@ -19,16 +21,13 @@ def neighbors(user: str = api.auth.user) -> pandas.DataFrame:
     etree = lxml.html.fromstring(httpx.get(url=f'https://www.last.fm/user/{user}/neighbours').text)
     users = [user.text for user in etree.cssselect(expr='a.user-list-link')]
     artists = [[artist.text for artist in user.cssselect(expr='a')] for user in etree.cssselect(expr='p.user-list-shared-artists')]
-    return pandas.DataFrame({'user': users, 'artists_in_common': artists})
+    data = pandas.DataFrame({'user': users, 'artists_in_common': artists})
+    rich.console.Console().print(Recommended.table(data=data, title='Neighbors'))
 
 
 @dataclasses.dataclass
 class Recommended:
-    login_url: str = httpx.URL('https://www.last.fm/login')
     url: str = httpx.URL('https://www.last.fm/music/+recommended')
-    user_agent: str = os.getenv('USERAGENT', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36')
-    user: str = api.auth.user
-    password: str = os.getenv('LASTFM_PASSWORD')
 
     def __post_init__(self):
         self.client = self.login()
@@ -36,12 +35,22 @@ class Recommended:
     def login(self) -> httpx.Client:
         # [How to "log in" to a website using Python's Requests module?](https://stackoverflow.com/a/17633072)
         # [python-requests and django - CSRF verification failed. Request aborted](https://stackoverflow.com/a/20252654)
+        url = httpx.URL('https://www.last.fm/login')
         client = httpx.Client()
-        csrftoken = client.get(url=self.login_url).cookies.get('csrftoken')
-        data = dict(csrfmiddlewaretoken=csrftoken, username_or_email=self.user, password=self.password)
-        headers = httpx.Headers({'User-Agent': self.user_agent, 'Referer': str(self.login_url)})
-        response = client.post(url=self.login_url, data=data, headers=headers)
+        csrftoken = client.get(url=url).cookies.get('csrftoken')
+        data = dict(csrfmiddlewaretoken=csrftoken, username_or_email=api.auth.user, password=os.getenv('LASTFM_PASSWORD'))
+        user_agent = os.getenv('USERAGENT', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36')
+        headers = httpx.Headers({'User-Agent': user_agent, 'Referer': str(url)})
+        response = client.post(url=url, data=data, headers=headers)
         return client
+
+    @staticmethod
+    def table(data: pandas.DataFrame, title: str) -> rich.table.Table:
+        '''Print `data`: pandas.DataFrame as a `rich.table`.''' # [Convert a pandas.DataFrame object into a rich.Table object for stylized printing in Python.](https://gist.github.com/avi-perl/83e77d069d97edbdde188a4f41a015c4)
+        table = rich.table.Table(title=title, border_style='blue', header_style='bold', show_edge=False, row_styles=['dim', ''])
+        [table.add_column(str(col), max_width=120, no_wrap=True) for col in data.columns]
+        [table.add_row(*[str(x) for x in val]) for val in data.values]
+        return table
 
     def albums(self, page: int = 1) -> pandas.DataFrame:
         etree = lxml.html.fromstring(self.client.get(url=f'{self.url}/albums?page={page}').text)
@@ -51,7 +60,8 @@ class Recommended:
         listeners = [int(e.cssselect('div.music-recommended-albums-album-info p')[0].text.strip().replace(',', '')) for e in buffer]
         tracks = [int(e.cssselect('p.music-recommended-albums-album-info-item')[0].text.split(' ')[0]) if e.cssselect('p.music-recommended-albums-album-info-item') else 0 for e in buffer]
         similar = [[_.text for _ in e.cssselect('p.music-recommended-albums-album-context a')] for e in buffer]
-        return pandas.DataFrame(data={'albums': albums, 'artists': artists, 'listeners': listeners, 'tracks': tracks, 'similar': similar})
+        data = pandas.DataFrame(data={'albums': albums, 'artists': artists, 'listeners': listeners, 'tracks': tracks, 'similar': similar})
+        rich.console.Console().print(self.table(data=data, title=f'{self.__class__.__name__} Albums'))
 
     def artists(self, page: int = 1) -> pandas.DataFrame:
         etree = lxml.html.fromstring(self.client.get(url=f'{self.url}/artists?page={page}').text)
@@ -61,7 +71,8 @@ class Recommended:
         listeners = [int(e.cssselect('p.music-recommended-artists-artist-stats')[0].text.strip().replace(',', '')) for e in buffer]
         bio = [e.cssselect('p.factbox-summary')[0].text if e.cssselect('p.factbox-summary') else '' for e in buffer]
         similar = [[_.text for _ in e.cssselect('p.music-recommended-artists-context a')] for e in buffer]
-        return pandas.DataFrame(data={'artists': artists, 'tags': tags, 'listeners': listeners, 'bio': bio, 'similar': similar})
+        data = pandas.DataFrame(data={'artists': artists, 'tags': tags, 'listeners': listeners, 'bio': bio, 'similar': similar})
+        rich.console.Console().print(self.table(data=data, title=f'{self.__class__.__name__} Artists'))
 
     def rediscover(self, page: int = 1) -> pandas.DataFrame:
         etree = lxml.html.fromstring(self.client.get(url=f'{self.url}/rediscover?page={page}').text)
@@ -71,14 +82,16 @@ class Recommended:
         listeners = [int(e.cssselect('p.music-recommended-artists-artist-stats')[0].text.strip().replace(',', '')) for e in buffer]
         last_played = [e.cssselect('div.music-recommended-artists-context p')[0].text[12:] for e in buffer]
         listens = [int(e.cssselect('div.music-recommended-artists-context p')[1].text.split(' ')[0].replace(',', '')) for e in buffer]
-        return pandas.DataFrame(data={'artists': artists, 'tags': tags, 'listeners': listeners, 'last_played': last_played, 'listens': listens})
+        data = pandas.DataFrame(data={'artists': artists, 'tags': tags, 'listeners': listeners, 'last_played': last_played, 'listens': listens})
+        rich.console.Console().print(self.table(data=data, title='Rediscover'))
 
     def tags(self, page: int = 1) -> pandas.DataFrame:
         etree = lxml.html.fromstring(self.client.get(url=f'{self.url}/tags?page={page}').text)
         buffer = etree.cssselect(expr='div.col-main div.link-block')
         tags = [e.cssselect('h3 a')[0].text for e in buffer]
         artists = [[_.text for _ in e.cssselect('p a')] for e in buffer]
-        return pandas.DataFrame(data={'tags': tags, 'artists': artists})
+        data = pandas.DataFrame(data={'tags': tags, 'artists': artists})
+        rich.console.Console().print(self.table(data=data, title=f'{self.__class__.__name__} Tags'))
 
     def tracks(self, page: int = 1) -> pandas.DataFrame:
         etree = lxml.html.fromstring(self.client.get(url=f'{self.url}/tracks?page={page}').text)
@@ -87,4 +100,5 @@ class Recommended:
         artists = [e.cssselect('p a')[0].text for e in buffer]
         listeners = [int(e.cssselect('p.recommended-tracks-item-listeners')[0].text.strip().split(' ')[0].replace(',', '')) for e in buffer]
         similar = [[_.text for _ in e.cssselect('p.recommended-tracks-item-context a')] for e in buffer]
-        return pandas.DataFrame(data={'tracks': tracks, 'artists': artists, 'listeners': listeners, 'similar': similar})
+        data = pandas.DataFrame(data={'tracks': tracks, 'artists': artists, 'listeners': listeners, 'similar': similar})
+        rich.console.Console().print(self.table(data=data, title=f'{self.__class__.__name__} Tracks'))
